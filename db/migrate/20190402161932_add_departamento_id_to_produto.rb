63f8576b06bb2e95d11a5aaf324feb3a7c6563ec
alexsantosfilho <devalexsandro@gmail.com>
2019-04-02 17:30:48 -0300

    Update type

--- db/migrate/20190402161932_add_departamento_id_to_produto.rb
+++ db/migrate/20190402161932_add_departamento_id_to_produto.rb
@@ -1,5 +1,5 @@
 class AddDepartamentoIdToProduto < ActiveRecord::Migration[5.2]
   def change
-    add_column :produtos, :departamento_id
+    add_column :produtos, :departamento_id, :integer
   end
 end
