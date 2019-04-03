ff6e8ef1c6c937733766fc7ae10aa3c1d053c339
alexsantosfilho <devalexsandro@gmail.com>
2019-04-03 09:52:25 -0300

    Fix validates on department

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -2,6 +2,6 @@ class Produto < ApplicationRecord
 
     belongs_to :departamento, optional: true
 
-    validates :preco, presence: true
+    validates :preco, :departamento, presence: true
     validates_length_of :nome, :minimum=> 4
 end
